8d1d4a402360aa77436254d5617bc410229b14cb
Mitch Lillie <mitch.lillie@shopify.com>
2022-02-23 10:33:44 -0800

    add back in brew bundle for mac

--- setup-mac.sh
+++ setup-mac.sh
@@ -7,6 +7,8 @@ then
     cat vscode/extension_export | xargs -L 1 code --install-extension
     echo "Installing homebrew"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    echo "Installing homebrew bundle via Brewfile"
+    brew bundle
     echo "Installing ohmyzsh"
     ZSH="$(pwd)/ohmyzsh" ZSH_CUSTOM="$(pwd)/zsh-custom" sh ./ohmyzsh/tools/install.sh --unattended
     echo "I think you'll need to handle merging the zshrc with the pre.ohmyzsh version."
